64fcfa5dc12ab9326c4c0101dd45a494a4030b4a
Nastia232 <nastiav062@gmail.com>
2022-02-11 20:44:47 +0200

    my commit

---
+++ script/index.js
@@ -0,0 +1,47 @@
+// min
+let userName;
+let $name;
+let _name;
+let user_name;
+
+
+//var 2name;
+// var /age;
+// var class;
+// var case; s
+ /*var cg5hdj23;*/
+
+
+//norma
+let username = prompt("What is your name?");
+alert("Hello, " + username );
+
+let User_age = prompt("Please indicate your year of birth");
+const Year_ = 2022 - User_age;
+alert('Your approximate age - ' + Year_);
+
+var square = prompt("Enter the length of one side of the square");
+const Per = square *4;
+alert('Perimeter of the square '+ Per );
+
+//max
+
+let radius= prompt(" Specify the radius of the circle ");
+const P=3.14*(radius*2);
+alert('The radius of the circle is equal to '+P);
+
+let $cit= prompt("Specify the distance between the cities of departure and arrival");
+let $time= prompt("Specify the time for which you need to overcome the path ");
+const speed= $cit/$time ;
+alert('You need to move' +speed )
+
+let $sum= prompt("Specify the amount to be converted into euros ")
+const $ev=$sum/31.89
+alert('EUR'+$ev)
+
+
+
+
+
+
+
